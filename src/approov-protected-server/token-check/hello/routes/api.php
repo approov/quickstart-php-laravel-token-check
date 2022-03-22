b027b5e6a07c8293360f6447ba1037cfff84af43
Exadra37 <exadra37@gmail.com>
2022-03-22 19:32:44 +0000

    Upgraded the Approov token check server to use Laravel 9.5 in order to be able to run in PHP 8.
    Signed-off-by: Exadra37 <exadra37@gmail.com>

--- src/approov-protected-server/token-check/hello/routes/api.php
+++ src/approov-protected-server/token-check/hello/routes/api.php
@@ -14,12 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::group([], function () {
     Route::get('/', function () {
         return response()->json(['message' => 'Hello, World!']);
     });
 
-    Route::middleware('auth:api')->get('/user', function (Request $request) {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
         return $request->user();
     });
 });
